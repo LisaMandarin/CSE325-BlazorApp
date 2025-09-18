2b92a9cff8aec4825c65d98e299f166cac6ae430
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-09-18 16:13:05 +0800

    input binding

--- BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net9.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8814cdcebf69124c42a364bd7518898fb08efe01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76d304ff0c7d46256e102916228a7dc59e4da45a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
